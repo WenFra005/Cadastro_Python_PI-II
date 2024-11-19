df6ad01a4193d66a7e56747493738999364b317e
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-11-19 10:32:58 -0200

    implementa função de cadastro de cliente

--- cadastroPI_II.py
+++ cadastroPI_II.py
@@ -1,7 +1,7 @@
 import random
 
 # Bando de dados dos usuários (em memória)
-cliente = []
+clientes = []
 profissionais = []
 
 def exibirBoasVindas():
@@ -11,10 +11,33 @@ def exibirBoasVindas():
     
     Escolha uma das opções:
     
-    1. Cadastrar
-    2. Entrar como cliente
-    3. Entrar como funcionário
+    1. Cadastro de cliente
+    2. Cadastro de profissional
+    3. Entrar como cliente
+    4. Entrar como funcionário
+    5. Sair
           """)
 
+def cadastrarCliente():
+    nome = input("Nome: ")
+    email = input("Email: ")
+    cpf = input("CPF: ")
+    cep = input("CEP: ")
+    telefone = input("Telefone: ")
+    senha = input("Senha: ")
+    
+    cliente = {
+        "nome": nome,
+        "email": email,
+        "cpf": cpf,
+        "cep": cep,
+        "telefone": telefone,
+        "senha": senha
+    }
+    
+    clientes.append(cliente)
+    print("Cadastro de cliente realizado com sucesso!")
+    
+    
 
     
