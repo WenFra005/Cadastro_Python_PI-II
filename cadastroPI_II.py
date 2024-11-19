1082714af3f8a7db532745782155a458ad146724
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-11-19 10:03:37 -0200

    cria def de boas vindas

---
+++ cadastroPI_II.py
@@ -0,0 +1,11 @@
+def exibirBoasVindas():
+    print("""
+          Bem vindo ao FacilliBee!
+    Facilitamos o encontro do profissional para um serviço facilitado.
+    
+    Escolha uma das opções:
+    
+    1. Cadastrar
+    2. Entrar como cliente
+    3. Entrar como funcionário
+          """)
